8e2f24a90b30a5e4e807ae00069c1f2d48fdaf83
aCoherentJourney <aCoherentJourney@gmail.com>
2018-11-20 21:32:51 +0100

    push synthesis file

--- aCoherentJourney/synthesis.py
+++ aCoherentJourney/synthesis.py
@@ -2,9 +2,16 @@ import numpy as np
 import csv
 from scipy.io.wavfile import write
 
-def createSineWave(dur, freq, filename):
+dur = 1.
+freq = 440
+inputFilePath = "./../data/input/testData2.csv"
+
+def createSineWave(dur, freq, outputFilePath):
     # Volume regulation
     rquiet = 0.01
+
+    # Bit rate
+    nbit = 16
 
     # Samples per second
     sps = 44100
@@ -12,41 +19,28 @@ def createSineWave(dur, freq, filename):
     # Frequency / pitch of the sine wave
     freq_hz = freq
 
-    freq_hz_bass = 55.0
-
-
     # Duration
     duration_s = dur
 
     # Numpy magiiiic
     each_sample_number = np.arange(duration_s * sps)
     waveform = np.sin(2 * np.pi * each_sample_number * freq_hz / sps)
-    waveform_integers = np.int16(waveform * 32767)
+    waveform_integers = np.int16(waveform * 2**(nbit-1)-1)
 
     # Output
-    write(filename, sps, waveform_integers)
+    write(outputFilePath, sps, waveform_integers)
 
 
+def getInputData(inputFilePath):
+    data = np.genfromtxt(inputFilePath, delimiter = ",")
+    #print(data)
+    return data
 
+def createSoundsFromFile(inputFilePath, outputFilePath):
+    data = getInputData(inputFilePath)
+    for i in range(len(data)):
+        dur = data[i,0]
+        freq = data[i,1]
+        createSineWave(dur, freq, "./../data/output/" + str(outputFilePath) + "" + str(i+1) + ".wav")
 
-
-
-
-
-def getInputData(filename):
-    ifile = open(filename, "rU")
-    reader = csv.reader(ifile, delimiter=",")
-
-    rownum = 0
-    a = []
-
-    for row in reader:
-        a.append(row)
-        rownum += 1
-
-
-    ifile.close()
-    return a
-
-
-    print(", ".join(a))
+createSoundsFromFile(inputFilePath, "testSounds")
