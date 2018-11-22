d7c2d2843d3a05b079ae76bf9bdd6b9552e46c58
aCoherentJourney <aCoherentJourney@gmail.com>
2018-11-22 21:40:17 +0100

    Data generator script improved

--- aCoherentJourney/datagen.py
+++ aCoherentJourney/datagen.py
@@ -3,18 +3,22 @@ import numpy as np
 
 Ndat = 50
 
-def randDataGen(x):
-    data = np.random.random([x,3])
-    return data
+def UniformRandDataGen(x):
+    dataRnd = np.random.random([x,3])
+    return dataRnd
 
+def ExpRandDataGen(x,y):
+    dataRnd = np.exp(-np.random.random([x,3])/y)
+    return dataRnd
 
 def orderDataGen(x):
+    dataOrd = np.random.random([x,3])
     for i in range(x):
-        data[i,0] = 1.
-        data[i,1] = ( 440 * 2**((i%36)/12) - 50) / (15000-50)
-        data[i,2] = i/50.
-    return data
+        dataOrd[i,0] = 1.
+        dataOrd[i,1] = ( 440 * 2**((i%36)/12) - 50) / (15000-50)
+        dataOrd[i,2] = i/50.
+    return dataOrd
 
 
-randDataGen(Ndat)
+data = ExpRandDataGen(Ndat,0.1)
 np.savetxt('testData4.csv',data, fmt = "%.5f", delimiter=",")
